149c925197a137f8170776e01214920254134344
qian <senshikou1985@gmail.com>
2021-08-27 22:10:02 +0900

    dw

--- mian.go
+++ mian.go
@@ -5,9 +5,9 @@ import (
 	"log"
 )
 
-var who = flag.String("who", "world", "Say hello to who")
+var hubd = flag.String("who", "world", "Say hello to who")
 
 func main() {
 	flag.Parse()
-	log.Println("Hello,", *who)
+	log.Println("Hello,", *hubd)
 }
